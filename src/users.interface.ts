c450831e3f14dac251fb80f4c8a2312b0e05df9b
Linto Cheeran <linto.cet@gmail.com>
2019-11-05 09:23:57 +0530

    remove unwanted prop

--- src/users.interface.ts
+++ src/users.interface.ts
@@ -2,7 +2,6 @@ import { Document } from 'mongoose';
 
 export interface usersInterface extends Document {
   _id: String,
-  favourites: Boolean,
   favourites_count: Number,
   friends: Boolean,
   friends_count: Number,
