bbd9d493bd45941883e0983ef623cca0073aaf5f
Linto Cheeran <linto.cet@gmail.com>
2020-02-08 00:03:16 +0530

    fix private pem env bug

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,7 +5,7 @@ import { env } from './env.validations';
 firebase.initializeApp({
   credential: firebase.credential.cert({
     clientEmail: env['FIREBASE_CLIENT_EMAIL'],
-    privateKey: env['FIREBASE_PRIVATE_KEY'],
+    privateKey: env['FIREBASE_PRIVATE_KEY'].replace(/\\n/g, '\n'),
     projectId: env['FIREBASE_PROJECT_ID'],
   }),
   databaseURL: 'https://linto-1.firebaseio.com',
