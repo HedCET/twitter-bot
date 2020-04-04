f7ef0b285470d7f8470c758df52b2c743419e929
Linto Cheeran <linto.cet@gmail.com>
2020-04-04 21:41:47 +0530

    prevent tweet from same user

--- src/scripts/kulukulu0033.ts
+++ src/scripts/kulukulu0033.ts
@@ -1,9 +1,5 @@
 module.exports = async ({ tweeter, status, twitter, user }) => {
-  if (
-    tweeter._id != user._id &&
-    (!tweeter.last_tweeted_at_frequency ||
-      30 < (tweeter.last_tweeted_at_frequency || 0))
-  ) {
+  if (tweeter._id != user._id && status.full_text.match(/സ്വാമിന.*/g)) {
     await twitter.post('statuses/retweet', { id: status.id_str });
     await new Promise(r => setTimeout(r, 1000 * 10));
   }
